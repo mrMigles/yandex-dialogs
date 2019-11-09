8d4d2e155dabaec89478385a1fff2f86ba76dfbb
mrMigles <delta_001@bk.ru>
2019-11-09 17:55:32 +0400

    Use gin framework

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module yandex-dialogs
 
 require (
+	github.com/gin-gonic/gin v1.4.0
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
--- voice_mail.go
+++ voice_mail.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"runtime/debug"
+	"github.com/gin-gonic/gin"
 )
 
 type VoiceMail struct {
 }
 
-func (handler VoiceMail) handleRequest() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if r := recover(); r != nil {
-				w.WriteHeader(500)
-				w.Write([]byte(fmt.Sprintf("{\"Error\": \"%+v\"}", r)))
-				fmt.Println("recovered from ", r)
-				debug.PrintStack()
-			}
-		}()
-		w.WriteHeader(200)
-		w.Write([]byte("Ok"))
+func (handler VoiceMail) handleRequest() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.String(200, "Ok")
 	}
 }
